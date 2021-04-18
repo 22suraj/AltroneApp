a386d33513e1063cf132fa9bfad53f4a17b16066
Abhinav Jha <abhinavjha98ald@gmail.com>
2021-04-18 14:16:09 +0100

    Added the feature of dark mode

---
+++ lib/services/add_service.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class AddService extends ChangeNotifier {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<bool> CreateFolder(Map data) async {
+    bool _success = false;
+    try {
+      _firestore.collection("Folder").add(data);
+      return _success = true;
+    } on FirebaseFirestore catch (e) {
+      print(e.toString());
+    }
+    print("$_success");
+    return _success;
+  }
+
+  deleteFolder(String id) {
+    try {
+      _firestore.collection("Folder").doc(id).delete();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
